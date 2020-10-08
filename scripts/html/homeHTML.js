cc585c3e68ec007809dfe7e57b7cada0542cce85
Matthew Nickolay <nickolaymatt@gmail.com>
2020-10-08 14:47:46 -0500

    working on home screen animation

--- scripts/html/homeHTML.js
+++ scripts/html/homeHTML.js
@@ -1,11 +1,18 @@
 const homeHTML = 
-`<div class="homeDiv">
-    <div class="nameDisplay container">
-        <div class="nameDisplayFirst">Matthew</div>
-        <div class="nameDisplaySecond">Nickolay</div>
+`
+<div class="designBar" id="homeDesignBar1"></div>
+<div class="designBar" id="homeDesignBar2"></div>
+<div class="designBar" id="homeDesignBar3"></div>
+<div class="designBar" id="homeDesignBar4"></div>
+<div class="designBar" id="homeDesignBar5"></div>
+<div class="homeDiv">
+    <div class="nameDisplay">
+        <div class="nameDisplayText">Matthew</div>
+        <div class="nameDisplayText">Nickolay</div>
     </div>
-    <div class="infoDisplay container">
+    <div class="infoDisplay">
         <div class="infoWebDisplay">&lt;Web Development&gt;</div>
         <div class="infoProgrammingDisplay">//Programming</div>
     </div>
-</div>`
+</div>
+`
